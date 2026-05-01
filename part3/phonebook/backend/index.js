4a450c75eadb1f04dfc3a239f6bcb22900d7fa4b
Patrick McKinney <git@prmckinney.com>
2026-05-01 15:34:13 -0500

    Exercises 3.13 - 3.14

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const Person = require('./models/person')
+
+
 const app = express()
 app.use(express.static('dist'))
-
 app.use(express.json())
 
 morgan.token('post', function (req, res) { return JSON.stringify(req.body) })
@@ -44,17 +47,15 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(person => {
+        response.json(person)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    if (person) {
+    Person.findById(request.params.id).then(person => {
         response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -63,6 +64,7 @@ app.delete('/api/persons/:id', (request, response) => {
 
     response.status(204).end()
 })
+
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -83,25 +85,24 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const person = {
+    const person = new Person({
         name: body.name,
-        number: body.number,
-        id: String(Math.floor(Math.random() * 1000000)),
-    }
+        number: body.number
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+    response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
 
